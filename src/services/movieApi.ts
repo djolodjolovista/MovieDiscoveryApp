761e961d86124f92267463072ad23429f03011f9
djolodjolo <tkd_djole@rocketmail.com>
2023-06-12 22:13:11 +0200

    Some corrections added env example

--- src/services/movieApi.ts
+++ src/services/movieApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const movieApi = createApi({
   reducerPath: 'movieApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_API}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}` }),
   endpoints: (builder) => ({
     getPopularMovies: builder.query({
       query: (page = 1) =>
